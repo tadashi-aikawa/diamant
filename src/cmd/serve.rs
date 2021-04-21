7dcd179d20c00b08f18ac6fa8384e427e8975b4b
tadashi-aikawa <syou.maman@gmail.com>
2021-04-22 00:26:02 +0900

    Add -p|--port options to `serve`

--- src/cmd/serve.rs
+++ src/cmd/serve.rs
@@ -1,12 +1,20 @@
 use clap::Clap;
+use rocket::config::{Config, Environment};
 
 use crate::api;
 
 #[derive(Clap, Debug)]
-pub struct Opts {}
+pub struct Opts {
+    #[clap(short, long, default_value = "8000")]
+    port: u16,
+}
 
-pub fn run(_opts: &Opts) {
-    rocket::ignite()
+pub fn run(opts: &Opts) {
+    let config = Config::build(Environment::active().unwrap_or(Environment::Development))
+        .port(opts.port)
+        .finalize()
+        .unwrap();
+    rocket::custom(config)
         .mount("/config", routes![api::config::index])
         .launch();
 }
